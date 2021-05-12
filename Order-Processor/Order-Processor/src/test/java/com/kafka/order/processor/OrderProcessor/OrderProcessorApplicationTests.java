513efffb9c9d40c8d8340f669e752ccb9d90afc3
FazulNoor <83147361+FazulNoor@users.noreply.github.com>
2021-05-12 12:23:11 +0530

    Project init for Order Processor

---
+++ Order-Processor/Order-Processor/src/test/java/com/kafka/order/processor/OrderProcessor/OrderProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.order.processor.OrderProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
