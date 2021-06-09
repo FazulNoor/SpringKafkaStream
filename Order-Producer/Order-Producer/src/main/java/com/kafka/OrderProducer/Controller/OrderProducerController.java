584caebde37bd3dee5d197148cec51bed352a369
FazulNoor <fazul_noor@yahoo.com>
2021-06-09 06:24:32 +0000

    add producer controller

---
+++ Order-Producer/Order-Producer/src/main/java/com/kafka/OrderProducer/Controller/OrderProducerController.java
@@ -0,0 +1,16 @@
+package com.kafka.OrderProducer.Controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderProducerController {
+    
+    @PutMapping("/ord/{orderId}")
+    public String sendOrder(@PathVariable String orderId){
+        return "Order Id Send ="+orderId;
+    }
+}
