2ed3ddd65038b66ce91132a9f0fae178848e77df
FazulNoor <fazul_noor@yahoo.com>
2021-06-10 15:43:40 +0000

    debug

--- Order-Consumer/Order-Consumer/src/main/java/com/kafka/order/consumer/config/OrderKafkaConfig.java
+++ Order-Consumer/Order-Consumer/src/main/java/com/kafka/order/consumer/config/OrderKafkaConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -22,22 +23,23 @@ public class OrderKafkaConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         log.info("Start consumerFactory>>>>");
         Map<String, Object> configMap = new HashMap();
-        configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
+        configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configMap.put(ConsumerConfig.GROUP_ID_CONFIG,"groupId");
         configMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        Log.info("End consumerFactory>>>>");
+        configMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        log.info("End consumerFactory>>>>");
         return new DefaultKafkaConsumerFactory<>(configMap);
 
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String,String> ordContainerFactory(){
-        Log.info("Start ordContainerFactory>>>>");
+        log.info("Start ordContainerFactory>>>>");
         ConcurrentKafkaListenerContainerFactory<String,String> conKafkaContFactory = new ConcurrentKafkaListenerContainerFactory<>();
         conKafkaContFactory.setConsumerFactory(consumerFactory());
         conKafkaContFactory.setConcurrency(3);
-        Log.info("End ordContainerFactory>>>>");
+        log.info("End ordContainerFactory>>>>");
         return conKafkaContFactory;
     }
 
